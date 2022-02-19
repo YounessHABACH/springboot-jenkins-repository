9c3b4aa7dfe4a695c1b306aefe3ef109c8dceff5
Youness HABACH <49674315+YounessHABACH@users.noreply.github.com>
2022-02-19 14:50:09 +0100

    Add files via upload

--- script.groovy
+++ script.groovy
@@ -1,12 +1,12 @@
-def buildApp(){
+def built-app(){
 	echo 'building the app - scrip groovy external'
 }
 
-def testApp(){
+def test-app(){
 	echo 'testing the app - scrip groovy external'
 }
 
-def deployApp(){
+def deploy-app(){
 	echo 'deploying the app - scrip groovy external'
 	echo "deploying ${params.VERSION}"
 }
